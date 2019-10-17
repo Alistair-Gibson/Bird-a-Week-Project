8cb5f21a584fe233b0a1629d1aaac77ea0f97273
Alistair Gibson <alistair111@gmail.com>
2019-10-17 11:02:37 +0100

    added bird images and text for first 4 weeks sorted issues with props calling images

--- src/birds.js
+++ src/birds.js
@@ -1,31 +1,31 @@
 export const birds = [{
     id: 1,
-    imgFile: "birdPlaceholder1",
-    title: "Week 1",
-    text: "Placeholder text Description of week 1 image"
+    picture: "nuthatch",
+    title: "Nuthatch (14.10)",
+    text: "The line work is getting stronger as are the proportions unsure about using hatching to shade or maybe introducing colour to get the areas of shadow, could maybe use a little more paint."
   },
   {
     id: 2,
-    imgFile: "birdPlaceholder2",
-    title: "Week 2",
-    text: "Placeholder text Description of week 2 image"
+    picture: "unkownred",
+    title: "unkown Red Bird(7.10)",
+    text: "Feeling a lot better about this one I want to slowly start reducing the amount of lines I use but the colour has worked a lot better in this one I want the splash effect without going overboard."
   },
   {
     id: 3,
-    imgFile: "birdPlaceholder3",
-    title: "Week 3",
-    text: "Placeholder text Description of week 3 image"
+    picture: "bluetit",
+    title: "Bluetit (30.09)",
+    text: "Happier with the proportions and line work on this one moved to actual watercolours and the effect is achievable but needs refining."
   },
   {
     id: 4,
-    imgFile: "birdPlaceholder4",
-    title: "Week 4",
-    text: "Placeholder text Description of week 4 image"
-  },
-  {
-    id: 5,
-    imgFile: "birdPlaceholder5",
-    title: "Week 5",
-    text: "Placeholder text Description of week 5 image"
+    picture: "robin",
+    title: "Robin (23.09)",
+    text: "The first bird in the project the proportions are a little off the watercolour pens I used did not have the opacity I was looking for, but a start is a start."
   }
+  // {
+  //   id: 5,
+  //   imgFile: "birdPlaceholder5",
+  //   title: "Week 5",
+  //   text: "Placeholder text Description of week 5 image"
+  // }
 ];
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import birdPlaceholder1 from "./birdPlaceholder1.png";
-// import birdPlaceholder2 from "./birdPlaceholder2.png";
-// import birdPlaceholder3 from "./birdPlaceholder3.png";
-// import birdPlaceholder4 from "./birdPlaceholder4.png";
-// import birdPlaceholder5 from "./birdPlaceholder5.png";
+// import birdPlaceholder1 from "./birdPlaceholder1.png";
+import bluetit from "./images/bluetit.png";
+import robin from "./images/robin.png";
+import unkownred from "./images/unkownred.png";
+import nuthatch from "./images/nuthatch.png";
 
-const Card = ({ picture, title, text, id }) => {
+const Card = props => {
   return (
     <div className="tc dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="birds" src={birdPlaceholder1} />
-      <div>
-        <h2>{title}</h2>
-        <p>{text}</p>
+      <img alt="birds" src={require(`./images/${props.picture}.png`)} />
+      <div className="card-text">
+        <h2>{props.title}</h2>
+        <p className="card-description">{props.text}</p>
       </div>
     </div>
   );
